30e2187b4be806d0429ba2373dea756441e0f74f
Fabiola Leon <fabiola.leon@ihg.com>
2017-03-20 22:16:43 -0400

    Adding assignment 1

---
+++ assignment1/scripts/app.js
@@ -0,0 +1,39 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope', '$filter'];
+function LunchCheckController($scope, $filter) {
+	$scope.food = "";
+	$scope.result = "";
+	$scope.message = "";
+	$scope.options = {
+		"too-much" : "Too much!",
+		"enjoy": "Enjoy!",
+		"empty": "Please enter data first"
+	};
+	
+	$scope.checkFood = function(){
+		var dishes = $scope.getDishes($scope.food);		
+		if(dishes.length === 0){
+			$scope.result = "empty";
+		}else if(dishes.length <= 3){
+			$scope.result = "enjoy";
+		}else{
+			$scope.result = "too-much";
+		}
+		$scope.message = $scope.options[$scope.result] ;
+	}
+	
+	$scope.getDishes = function(foodString){
+		if(typeof foodString !== "string" || foodString === ""){
+			return [];
+		}
+		return foodString.split(",");
+	}
+  
+}
+
+})();
